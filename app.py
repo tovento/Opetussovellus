112b7b2b68ad281d5f69f72bf4e62e4ab3548c24
tovento <92781588+tovento@users.noreply.github.com>
2022-09-14 09:57:09 +0300

    Create first version, login and logout

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template, redirect, request, session
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username & password
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
